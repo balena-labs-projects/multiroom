ab8435f5a3f9c4c2c463d8b67e9ff7ccefdc499b
Tomás Migone <tomas@balena.io>
2021-06-02 12:11:47 -0300

    Update block to allow snapclient usage
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- entry.sh
+++ entry.sh
@@ -4,21 +4,38 @@ set -e
 # Run balena base image entrypoint script
 /usr/bin/entry.sh echo ""
 
-# Environment variables and defaults
-MULTI_ROOM_MODE="${MULTI_ROOM_MODE-SERVER}"
+# Get mode
+if [[ "$@" == *"snapclient"* ]]; then
+  MULTI_ROOM_MODE="client"
+else
+  MULTI_ROOM_MODE="server"
+fi
 
 echo "--- Multiroom ---"
 echo "Starting multiroom service with settings:"
 echo "- Mode: $MULTI_ROOM_MODE"
-echo "- Snapcast version: $(snapclient --version | head -n 1 | cut -d' ' -f2)"
+echo "- Snapcast version: $(snapserver --version | head -n 1 | cut -d' ' -f2)"
 
-# If command starts with an option, prepend snap to it
-if [[ "${1#-}" != "$1" ]]; then
-  if [[ "{$MULTI_ROOM_MODE}" == "CLIENT" ]]; then
-    set -- snapclient "$@"
-  else
-    set -- snapserver "$@"
+# Add defaults to client
+if [[ "${MULTI_ROOM_MODE}" == "client" ]]; then
+
+  if [[ ! "$@" == *"logfilter"* ]]; then
+    set -- "$@" --logfilter=*:error
   fi
+
+  if [[ ! "$@" == *"hostID"* ]]; then
+    set -- "$@" --hostID=$BALENA_DEVICE_UUID
+  fi
+
+  if [[ ! "$@" == *"--host="* ]]; then
+    set -- "$@" --host=multiroom-server
+  fi
+
+fi
+
+# If command starts with an option, assume snapserver and prepend snap to it
+if [[ "${1#-}" != "$1" ]]; then
+  set -- snapserver "$@"
 fi
 
 exec "$@"
